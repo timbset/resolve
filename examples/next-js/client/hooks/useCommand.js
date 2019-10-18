463f8f3e1d3866aaf85f4943576d8285cac1a0ef
timbset <timbset@gmail.com>
2019-10-18 14:05:39 +0300

    Delete unnecessary code

--- examples/next-js/client/hooks/useCommand.js
+++ examples/next-js/client/hooks/useCommand.js
@@ -9,13 +9,6 @@ const useCommand = () => {
     });
   }, []);
 
-  // const command = {
-  //   "type":"createObjective",
-  //   "aggregateId":"4c46fe8d-9a00-42ac-8bb5-cf4708d0b6fe",
-  //   "aggregateName":"objective",
-  //   "payload":{"title":"New Objective","orgUnitId":"d9c50423-c9c9-49cc-a2f6-440c49622fb0","period":"19.2"}
-  // }
-
   return [executeCommand];
 };
 
